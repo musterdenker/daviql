d65fea32acbde64024ba9282aece9dcd0a9c4e9c
Mathias Fiedler <mathias.fiedler@artflakes.com>
2012-10-26 09:46:29 +0200

    enable twitter bootstrap and restricting app to user only

--- app/controllers/facade_controller.rb
+++ app/controllers/facade_controller.rb
@@ -1,5 +1,5 @@
 class FacadeController < ApplicationController
-
+	before_filter :authenticate_user!
 
 def start
 	@queries = Query.all
--- app/controllers/interpreter/base_controller.rb
+++ app/controllers/interpreter/base_controller.rb
@@ -1,4 +1,4 @@
 class Interpreter::BaseController < ApplicationController
-	
+	before_filter :authenticate_user!	
 
 end
