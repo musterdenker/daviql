52e00aebbe8002fcf2df348146a148f19f6b1e2e
Mathias Fiedler <mathias@musterdenker.de>
2016-02-20 12:43:48 +0100

    adding list js for start page card searching WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require tether
 //= require bootstrap
+//= list
 //= require dataTables/jquery.dataTables
 //= require dataTables.bootstrap4.min
 //= require dataTables/extras/dataTables.tableTools
