456b64b78bd859d9e7e2f86a5f3e705881f2e505
Mathias Fiedler <mathias@musterdenker.de>
2016-02-21 22:57:57 +0100

    making new queries work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def get_codemirror_mime_type query
-    DatabaseTypes.all[query.data_source.database_type.to_sym][:"mime-type"]
+    if query.data_source
+      DatabaseTypes.all[query.data_source.database_type.to_sym][:"mime-type"]
+    else
+      "text/x-sql"
+    end
   end
 end
